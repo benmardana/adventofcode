b456c7660f09d076f6f6db8f9393ffe517fad9bf
Ben McGarvey <b.mcgarvey.bm@gmail.com>
2023-08-08 10:17:47 +1000

    day_eight

--- src/solutions/day_eight.rs
+++ src/solutions/day_eight.rs
@@ -1,5 +1,3 @@
-use std::cmp;
-
 pub fn solve(input: String) -> Option<(i32, i32)> {
     let trees = input
         .lines()
@@ -9,41 +7,55 @@ pub fn solve(input: String) -> Option<(i32, i32)> {
                 .collect::<Vec<_>>()
         })
         .collect::<Vec<_>>();
-    let mut visible_trees = 0;
-    for y in 0..trees.len() {
-        for x in 0..trees[y].len() {
-            dbg!(trees[y][x]);
+    let mut visible_trees = ((trees.len() - 2) * 2) + (trees[0].len() * 2);
+    for y in 1..trees.len() - 1 {
+        for x in 1..trees[y].len() - 1 {
             if is_visible(&trees, x, y) {
                 visible_trees += 1;
-                dbg!(true);
-            } else {
-                dbg!(false);
             }
         }
     }
-    Some((visible_trees, 0))
+    Some((visible_trees.try_into().unwrap(), 0))
+}
+
+fn score(input: &Vec<Vec<u32>>, x: usize, y: usize) -> u8 {
+    let target_tree: u32 = input[y][x];
+    let mut view_length = 0;
+    for tree in input[0..y].iter().rev().map(|row| row[x]) {
+        view_length += 1;
+        if tree >= target_tree {
+            break;
+        }
+    }
+    view_length
 }
 
 fn is_visible(input: &Vec<Vec<u32>>, x: usize, y: usize) -> bool {
     let target_tree: u32 = input[y][x];
 
     // up
-    if ((y.saturating_sub(1))..=0).all(|iy| input[iy][x] < target_tree) {
+    if input[0..y].iter().map(|row| row[x]).max().unwrap() < target_tree {
         return true;
     }
 
     // down
-    if (cmp::min(y + 1, input.len() - 1)..input.len()).all(|iy| input[iy][x] < target_tree) {
+    if input[y + 1..input.len()]
+        .iter()
+        .map(|row| row[x])
+        .max()
+        .unwrap()
+        < target_tree
+    {
         return true;
     }
 
     // left
-    if ((x.saturating_sub(1))..=0).all(|ix| input[y][ix] < target_tree) {
+    if input[y][0..x].iter().max().unwrap() < &target_tree {
         return true;
     }
 
     // right
-    if (cmp::min(x + 1, input.len() - 1)..input[0].len()).all(|ix| input[y][ix] < target_tree) {
+    if input[y][x + 1..input[y].len()].iter().max().unwrap() < &target_tree {
         return true;
     }
 
