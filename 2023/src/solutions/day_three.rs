bad2ba46700dd47eccf64e8c36eb19bd50fbcff6
Ben Mardana <ben.mardana@gmail.com>
2023-12-11 08:44:41 +1100

    3wip

--- 2023/src/solutions/day_three.rs
+++ 2023/src/solutions/day_three.rs
@@ -1,4 +1,4 @@
-use std::cmp::min;
+use std::{cmp::min, collections::HashMap};
 
 use super::Solution;
 
@@ -12,22 +12,44 @@ impl Solution for DayThree {
         input.raw.lines().enumerate().for_each(|(i, line)| {
             let nums = num_re.find_iter(line);
             for num in nums {
-                let above = 'above: {
-                    if i == 0 {
-                        break 'above "".to_owned();
-                    }
-                    dbg!(input.raw.lines().nth(i - 1).unwrap_or("").get(num.start().saturating_sub(1)..(min(num.end() + 1, line.len() + 1))));
-                    input.raw.lines().nth(i - 1).unwrap_or("").get(num.start().saturating_sub(1)..(min(num.end() + 1, line.len()))).unwrap_or("").to_string()
-                };
-                let middle = format!("{}{}", line.get(num.start().saturating_sub(1)..num.start()).unwrap_or(""), line.get(num.end()..(min(num.end() + 1, line.len()))).unwrap_or(""));
+                let above =
+                    'above: {
+                        if i == 0 {
+                            break 'above "".to_owned();
+                        }
+                        dbg!(input.raw.lines().nth(i - 1).unwrap_or("").get(
+                            num.start().saturating_sub(1)..(min(num.end() + 1, line.len() + 1))
+                        ));
+                        input
+                            .raw
+                            .lines()
+                            .nth(i - 1)
+                            .unwrap_or("")
+                            .get(num.start().saturating_sub(1)..(min(num.end() + 1, line.len())))
+                            .unwrap_or("")
+                            .to_string()
+                    };
+                let middle = format!(
+                    "{}{}",
+                    line.get(num.start().saturating_sub(1)..num.start())
+                        .unwrap_or(""),
+                    line.get(num.end()..(min(num.end() + 1, line.len())))
+                        .unwrap_or("")
+                );
                 let below = 'below: {
                     if i == input.raw.len() + 1 {
                         break 'below "".to_owned();
                     }
-                    input.raw.lines().nth(i + 1).unwrap_or("").get(num.start().saturating_sub(1)..(min(num.end() + 1, line.len()))).unwrap_or("").to_string()
+                    input
+                        .raw
+                        .lines()
+                        .nth(i + 1)
+                        .unwrap_or("")
+                        .get(num.start().saturating_sub(1)..(min(num.end() + 1, line.len())))
+                        .unwrap_or("")
+                        .to_string()
                 };
                 let context = above + &middle + &below;
-                dbg!(num, &context);
                 if symbol_re.is_match(&context) {
                     sum = sum + num.as_str().parse::<u64>().unwrap()
                 }
@@ -37,34 +59,56 @@ impl Solution for DayThree {
     }
 
     fn part_two(&self, input: &crate::input::Input) -> String {
-        let gear_re = regex::Regex::new(r"\*").unwrap();
-        let symbol_re = regex::Regex::new(r"[^0-9\.]").unwrap();
-        let mut sum: u64 = 0;
+        let num_re = regex::Regex::new(r"\d+").unwrap();
+        let symbol_re = regex::Regex::new(r"\*]").unwrap();
+        let map: HashMap<String, Vec<u64>> = HashMap::new();
         input.raw.lines().enumerate().for_each(|(i, line)| {
-            let gears = gear_re.find_iter(line);
-            for gear in gears {
-                let above = 'above: {
-                    if i == 0 {
-                        break 'above "".to_owned();
-                    }
-                    dbg!(input.raw.lines().nth(i - 1).unwrap_or("").get(num.start().saturating_sub(1)..(min(num.end() + 1, line.len() + 1))));
-                    input.raw.lines().nth(i - 1).unwrap_or("").get(num.start().saturating_sub(1)..(min(num.end() + 1, line.len()))).unwrap_or("").to_string()
-                };
-                let middle = format!("{}{}", line.get(num.start().saturating_sub(1)..num.start()).unwrap_or(""), line.get(num.end()..(min(num.end() + 1, line.len()))).unwrap_or(""));
+            let nums = num_re.find_iter(line);
+            for num in nums {
+                let above =
+                    'above: {
+                        if i == 0 {
+                            break 'above "".to_owned();
+                        }
+                        dbg!(input.raw.lines().nth(i - 1).unwrap_or("").get(
+                            num.start().saturating_sub(1)..(min(num.end() + 1, line.len() + 1))
+                        ));
+                        input
+                            .raw
+                            .lines()
+                            .nth(i - 1)
+                            .unwrap_or("")
+                            .get(num.start().saturating_sub(1)..(min(num.end() + 1, line.len())))
+                            .unwrap_or("")
+                            .to_string()
+                    };
+                let middle = format!(
+                    "{}{}",
+                    line.get(num.start().saturating_sub(1)..num.start())
+                        .unwrap_or(""),
+                    line.get(num.end()..(min(num.end() + 1, line.len())))
+                        .unwrap_or("")
+                );
                 let below = 'below: {
                     if i == input.raw.len() + 1 {
                         break 'below "".to_owned();
                     }
-                    input.raw.lines().nth(i + 1).unwrap_or("").get(num.start().saturating_sub(1)..(min(num.end() + 1, line.len()))).unwrap_or("").to_string()
+                    input
+                        .raw
+                        .lines()
+                        .nth(i + 1)
+                        .unwrap_or("")
+                        .get(num.start().saturating_sub(1)..(min(num.end() + 1, line.len())))
+                        .unwrap_or("")
+                        .to_string()
                 };
-                let context = above + &middle + &below;
-                dbg!(num, &context);
-                if symbol_re.is_match(&context) {
-                    sum = sum + num.as_str().parse::<u64>().unwrap()
+                if let Some(m) = symbol_re.find(&above) {
+                    let x = m.start() + num.start();
+                    let y = i - 1;
+                    num.as_str().parse::<u64>().unwrap()
                 }
             }
         });
         sum.to_string()
     }
 }
-
